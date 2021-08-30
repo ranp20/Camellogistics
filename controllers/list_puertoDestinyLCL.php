03f22344b2feb7c4a31b545a1a6b6227c3b482b8
ranp20 <polancoranp2019@gmail.com>
2021-08-30 17:56:48 -0500

    new changes - repair input second for port destiny

--- controllers/list_puertoDestinyLCL.php
+++ controllers/list_puertoDestinyLCL.php
@@ -8,16 +8,16 @@ class list_puertoDestinyLCL extends Connection{
 		try{
 			$sql = "SELECT tap.puerto_id as 'idpuerto', tap.puerto as 'puerto', tapa.pais_id as 'idpais', tapa.pais as 'pais' 
 								FROM tbl_aq_puertos tap	INNER JOIN tbl_aq_paises tapa ON tap.pais_id = tapa.pais_id 
-								WHERE tap.pais_id != '".$id."' ORDER BY tap.puerto_id DESC";
+								WHERE tap.pais_id = 2 ORDER BY tap.puerto_id DESC";
 
 			if(isset($_POST['searchList'])){
 				//$search = $this->con->real_escape_string($_POST['searchList']);
-				$search = addslashes($_POST['searchList']);
+				//$search = addslashes($_POST['searchList']);
 				$sql = "SELECT tap.puerto_id as 'idpuerto', tap.puerto as 'puerto', tapa.pais_id as 'idpais', tapa.pais as 'pais' 
 								FROM tbl_aq_puertos tap	INNER JOIN tbl_aq_paises tapa ON tap.pais_id = tapa.pais_id 
-								WHERE tap.puerto LIKE '%".$search."%' OR
-											tapa.pais LIKE '%".$search."%'
-								AND tap.pais_id != '".$id."'
+								WHERE tap.pais_id = 2 
+											AND	tap.puerto LIKE '%CALLAO%' OR
+													tapa.pais LIKE '%CALLAO%'
 								ORDER BY tap.puerto_id DESC";
 			}
 
