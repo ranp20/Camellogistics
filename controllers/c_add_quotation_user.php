99144489e12d35f4e8c834854bbb4af5d6645e35
ranp20 <polancoranp2019@gmail.com>
2021-10-01 20:46:43 -0500

    new changes - advanced minimal

--- controllers/c_add_quotation_user.php
+++ controllers/c_add_quotation_user.php
@@ -4,7 +4,23 @@ class Add_Quotation_User extends Connection{
 	function add($arr_userdata){
 
 		try{
-			$sql = "CALL sp_add_quotation_user()";
+			$sql = "CALL sp_add_quotation_user(
+			"ranppuntos20@gmail.com", 
+			"TRANSPORTE MARÍTIMO", 
+			"MARÍTIMO", 
+			"CARGA GENERAL", 
+			"SERVICES S.A.C.", 
+			"990124256", 
+			"TRANSPORTE MARÍTIMO", 
+			"CARGA GENERAL", 
+			"QINGDAO-CHINA", 
+			"3000Kg/1.08CBM", 
+			"30 Días", 
+			"45000", 
+			"1702.96", 
+			"120.35", 
+			"47329.26"
+			)";
 			$stm = $this->con->prepare($sql);
 			foreach ($arr_userdata as $key => $value) {
 				$stm->bindValue($key, $value);
