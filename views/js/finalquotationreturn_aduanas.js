55ab513f55b5d9b2cde0eb75c54e33957f3fcf88
ranp20 <polancoranp2019@gmail.com>
2022-03-18 15:52:53 -0500

    add notification into resume quotation

--- views/js/finalquotationreturn_aduanas.js
+++ views/js/finalquotationreturn_aduanas.js
@@ -13,6 +13,32 @@ function twodecimals(n){
 function firstToUppercase(e) {
   return e.charAt(0).toUpperCase() + e.slice(1);
 }
+// ========== NOTIFICACIONES ========== //
+function showNotification(typeNotify){
+	var htmlNotify = "";
+	if(typeNotify == "true"){
+		htmlNotify = `<div id="notiMsgAlertCli">
+      <div class="c_notifyMsg toast--green">
+        <div class="c_notifyMsg__icon">
+          <svg version="1.1" class="c_notifyMsg__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+          <g><g><path d="M504.502,75.496c-9.997-9.998-26.205-9.998-36.204,0L161.594,382.203L43.702,264.311c-9.997-9.998-26.205-9.997-36.204,0    c-9.998,9.997-9.998,26.205,0,36.203l135.994,135.992c9.994,9.997,26.214,9.99,36.204,0L504.502,111.7    C514.5,101.703,514.499,85.494,504.502,75.496z"></path>
+          </g></g>
+          </svg>
+        </div>
+        <div class="c_notifyMsg__content">
+          <p class="c_notifyMsg__type">Success</p>
+          <p class="c_notifyMsg__message">Anyone with access can view your invited visitors.</p>
+        </div>
+        <div class="c_notifyMsg__close">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.642 15.642" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 15.642 15.642">
+          <path fill-rule="evenodd" d="M8.882,7.821l6.541-6.541c0.293-0.293,0.293-0.768,0-1.061  c-0.293-0.293-0.768-0.293-1.061,0L7.821,6.76L1.28,0.22c-0.293-0.293-0.768-0.293-1.061,0c-0.293,0.293-0.293,0.768,0,1.061  l6.541,6.541L0.22,14.362c-0.293,0.293-0.293,0.768,0,1.061c0.147,0.146,0.338,0.22,0.53,0.22s0.384-0.073,0.53-0.22l6.541-6.541  l6.541,6.541c0.147,0.146,0.338,0.22,0.53,0.22c0.192,0,0.384-0.073,0.53-0.22c0.293-0.293,0.293-0.768,0-1.061L8.882,7.821z"></path>
+          </svg>
+        </div>
+      </div>
+    </div>`;
+	}
+	return htmlNotify;
+}
 $(document).ready(function(){
 	// ========== VALIDAR SI EXISTE UN USUARIO, DE LO CONTRARIO ASIGNAR EL USUARIO POR DEFECTO ========== //
 	if($("#s_useregin-sistem").val() == "" || $("#s_useregin-sistem").val() == undefined || $("#s_useregin-sistem").val() == 'undefined' || $("#s_useregin-sistem").val() == null || $("#s_useregin-sistem").val() == 'null'){
@@ -1176,10 +1202,22 @@ $(document).ready(function(){
 		        cache: false,
 		        processData: false
 					}).done(function(e){
+						var notifyMessage = e;
 						if(e == "true"){
 							$("#cnt-modalFormLoginyRegister").add($(".cnt-modalFormLoginyRegister--c")).removeClass("show");
 							$("#cUIMessageValid-user").html("");
 							console.log('Cotización guardada en unlogged');
+
+							$("#cUIMessageValid-user").html(showNotification(notifyMessage));
+
+							$(document).on("click", ".c_notifyMsg__close", function(e){
+						    e.preventDefault();
+						    var parent = $(this).parent().parent("#notiMsgAlertCli");
+						    parent.fadeOut("slow", function() { 
+						    	$(this).remove(); 
+						   	});
+						  });
+
 						}else{
 							console.log("Error al guardar la información");
 						}
