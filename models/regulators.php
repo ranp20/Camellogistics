ceb58ea65d2a230b1ee7b0993bc356a4e4c0aac0
ranp20 <polancoranp2019@gmail.com>
2022-03-21 22:55:46 -0500

    PHP secutity additions

--- models/regulators.php
+++ models/regulators.php
@@ -4,32 +4,34 @@ class Regulator extends Connection
 {
   private $table = "tbl_regulators";
 
-  function __construct()
+  public function __construct()
   {
     parent::__construct();
   }
 
   /************************** DEVOLVER NÚMERO TOTAL DE REGISTROS **************************/
-  function get_count_regulators(){
-    try{
+  public function get_count_regulators()
+  {
+    try {
       $sql = "SELECT count(*) as numrows FROM {$this->table}";
       $stm = $this->con->prepare($sql);
       $stm->execute();
       return $stm->fetchAll(PDO::FETCH_ASSOC);
-    }catch(PDOEXception $e){
+    } catch (PDOEXception $e) {
       return $e->getMessage();
     }
   }
   /************************** LIMITAR EL NÚMERO DE REGISTROS POR VISTA **************************/
-  function get_limit_regulators($offset, $per_page){
-    try{
+  public function get_limit_regulators($offset, $per_page)
+  {
+    try {
       $sql = "CALL sp_limit_Regulators(:offset, :per_page)";
-      $stm = $this->con->prepare($sql);            
+      $stm = $this->con->prepare($sql);
       $stm->bindValue(":offset", $offset);
       $stm->bindValue(":per_page", $per_page);
       $stm->execute();
       return $stm->fetchAll(PDO::FETCH_ASSOC);
-    }catch(PDOEXception $e){
+    } catch (PDOEXception $e) {
       return $e->getMessage();
     }
   }
