43e7384ce2a945910b5e7fb192ba5e1492ee58af
ranp20 <polancoranp2019@gmail.com>
2022-06-02 12:44:04 -0500

    repair structure of files into admin

--- admin/views/includes/adm-header-index.php
+++ admin/views/includes/adm-header-index.php
@@ -2,25 +2,33 @@
   $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
   $url =  $actual_link . "/Camellogistics/admin/views/";
 ?>
-<meta charset="utf-8" />
-<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no"/>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<meta name="description" content=""/>
-<meta name="author" content=""/>
+<meta charset="UTF-8">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+<meta http-equiv="Pragma" content="no-cache"/>
+<meta http-equiv="Expires" content="0"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no, viewport-fit=cover">
 <meta name="description" content="¡Calcula el costo de tu importación en 4 simples pasos!"/>
+<meta name="theme-color" content="#B58440">
+<meta name="author" content="R@np-2021"/>
 <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+<meta name="twitter.card" content="summary">
 <meta property="og:locale" content="es_ES"/>
-<meta property="og:type" content="article"/>
-<meta property="og:title" content="Calculadora de Envíos | Camel"/>
-<meta property="og:description" content="¡Calcula el costo de tu importación en 4 simples pasos!"/>
-<meta property="og:site_name" content="Camel"/>
-<link rel="icon" type="image/x-icon" href="<?= $url ?>assets/img/utilities/favicon-camel.png"/>
-<link rel="stylesheet" href="<?= $url ?>assets/css/styles.min.css">
-<!-- JQUERY -->
-<script src="<?= $url ?>js/jquery-3.6.0.min.js"></script>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="Camel Logistics | Admin"/>
+<meta property="og:url" name="twitter.url" content="https://localhost/Camellogistics">
+<meta property="og:title" name="twitter.title" content="Calculadora de Envíos | Camel Logistics"/>
+<meta property="og:description" name="twitter.description" content="¡Calcula el costo de tu importación en 4 simples pasos!"/>
+<meta property="og:image" name="twitter.image" content="<?= $url; ?>assets/img/logos/logotipo-camel.png"/>
+<link rel="icon" type="image/x-icon" href="<?= $url; ?>assets/img/favicon/favicon-camel.png"/>
+<!-- JQUERY COMPRESSED -->
+<script src="<?= $url; ?>js/plugins/jquery/jquery-3.6.0.min.js"></script>
 <!-- BOOTSTRAP UNCOMPRESSED -->
-<link rel="stylesheet" href="<?php echo $url ?>js/bootstrap/css/bootstrap.min.css">
-<script src="<?php echo $url ?>js/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="<?php echo $url; ?>js/plugins/bootstrap/css/bootstrap.min.css">
+<script src="<?php echo $url; ?>js/plugins/bootstrap/js/bootstrap.min.js"></script>
+<!-- STYLESSHEET -->
+<link rel="stylesheet" href="<?= $url; ?>assets/css/styles.min.css">
 <!-- GOOGLE FONTS -->
 <!-- 
 <link rel="preconnect" href="https://fonts.googleapis.com">
