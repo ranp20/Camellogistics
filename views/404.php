b807f11381b9b61490fa9d4cd894f2efc956a020
ranp20 <polancoranp2019@gmail.com>
2022-06-23 16:37:57 -0500

    add page 404

---
+++ views/404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Memopay |</title>
+	<link rel="stylesheet" href="views/assets/css/styles.min.css">
+</head>
+<body>
+	<div class="cErr-404_cont">
+		<section class="cErr-404_cont__sbanner"></section>
+		<section class="cErr-404_cont__desc">
+			<h1>404</h1>
+			<h2>Este espacio no está habilitado.</h2>
+			<p>Regresa al inicio y descubre todas las soluciones logísticas que <span class="bold-pricolor">CAMELLOGISTICS</span> tiene para ti.</p>
+			<a href="/Camellogistics">Volver al inicio</a>
+		</section>
+	</div>
+</body>
+</html>
