75a37c9430e7e9afd0a8bd9c87dc11128b2c0bd1
ranp20 <polancoranp2019@gmail.com>
2022-07-02 11:30:39 -0500

    repair login into admin

--- admin/controllers/prcss_login-adm.php
+++ admin/controllers/prcss_login-adm.php
@@ -1,7 +1,7 @@
 <?php
-if (isset($_POST) && count($_POST) > 0) {
-  if (preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST['adm-log-email'])) {
-
+$r = "";
+if(isset($_POST) && count($_POST) > 0){
+  if(preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST['adm-log-email'])){
     $arr_logiadm = [
       "email"    => $_POST['adm-log-email'],
       "password" => $_POST['adm-log-pass'],
@@ -11,57 +11,52 @@ if (isset($_POST) && count($_POST) > 0) {
     require_once 'c_login-adm.php';
     $login  = new Login_Adm();
     $verify = $login->Login($arr_logiadm);
-
-    if (!empty($verify) && $verify == "true") {
+    if(!empty($verify) && $verify == "true"){
       require_once 'c_list-by-admin.php';
-
       $emailadm = $arr_logiadm['email'];
-
       $user       = new List_byIdAdmin();
       $getbyemail = $user->list($emailadm);
 
-      if (count($getbyemail) > 0) {
+      if(count($getbyemail) > 0){
         // INICIAR SESIÓN
         session_start();
         $_SESSION['admin_camel'] = $getbyemail[0];
 
-        if ($chkrempass == "on") {
+        if($chkrempass == "on"){
           $resadm_email = $getbyemail[0]['email'];
           $resadm_pass  = $getbyemail[0]['password'];
 
           // CREAR COOKIES
           $cookie_expiration_time = time() + (10 * 365 * 24 * 60 * 60); // COOKIE EXPIRA EN 10 AÑOS
-          if (count($_COOKIE) > 0) {
+          if(count($_COOKIE) > 0){
             setcookie("adm-email", json_encode($resadm_email), $cookie_expiration_time, '/', '', true, true);
             setcookie("adm-password", json_encode($resadm_pass), $cookie_expiration_time, '/', '', true, true);
-          } else {
+          }else{
             //echo "Cookies NO soportadas";
           }
         }
-
-        $res = array(
-          'response' => 'true',
+        $r = array(
+          'res' => 'true',
           'received' => $getbyemail[0],
         );
-      } else {
-
-        $res = array(
-          'response' => 'false',
+      }else{
+        $r = array(
+          'res' => 'false',
         );
       }
-    } else {
-      $res = array(
-        'response' => 'false',
+    }else{
+      $r = array(
+        'res' => 'false',
       );
     }
-  } else {
-    $res = array(
-      'response' => 'error_email',
+  }else{
+    $r = array(
+      'res' => 'error_email',
     );
   }
-} else {
-  $res = array(
-    'response' => 'false',
+}else{
+  $r = array(
+    'res' => 'false',
   );
 }
-die(json_encode($res));
+die(json_encode($r));
