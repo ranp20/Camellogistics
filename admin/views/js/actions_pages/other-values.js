7280a84afd386973c6287ff5ecdcd27369922a32
ranp20 <polancoranp2019@gmail.com>
2022-07-05 17:16:43 -0500

    add 'cantidad a validar' into admin

--- admin/views/js/actions_pages/other-values.js
+++ admin/views/js/actions_pages/other-values.js
@@ -3,7 +3,7 @@ $(() => {
   list_certiconform();
 });
 // ------------ LIMITAR A DOS DECIMALES CUALQUIER INPUT DE TIPO NÚMERO EN DONDE ESTÁ IMPORTADO ESTE ARCHIVO
-$(document).on("input","input[data-valformat='twodecimal']",function(e){
+$(document).on("keyup keypress","input[data-valformat='twodecimal']",function(e){
 	let val = e.target.value;
   let t = val.toString();
   let regex = /(\d*.\d{0,2})/;
@@ -11,9 +11,15 @@ $(document).on("input","input[data-valformat='twodecimal']",function(e){
   $(this).val(vl_twoformat);
 });
 // ------------ FORMATO - SEPARADOR DE MILLAR Y PUNTO DECIMAL
-$(document).on("keyup", "input[data-valformat=withcomedecimal]", function(e){
+$(document).on("keyup keypress", "input[data-valformat=withcomedecimal]", function(e){
   let val = e.target.value;
   let val_formatNumber = val.toString().replace(/[^\d.]/g, "").replace(/^(\d*\.)(.*)\.(.*)$/, '$1$2$3').replace(/\.(\d{2})\d+/, '.$1').replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  $(this).val(val_formatNumber);
+});
+// ------------ FORMATO - SOLO VALORES ENTEROS Y SIN NEGATIVOS
+$(document).on("keyup keypress", "input[data-valformat=onlyintegers]", function(e){
+  let val = e.target.value;
+  let val_formatNumber = val.toString().replace(/[^\d]/g, "");
   $(this).val(val_formatNumber);
 });
 // ------------ LISTAR LOS SEGUROS DE TRANSPORTE MARÍTIMO Y AÉREO
@@ -120,28 +126,53 @@ function list_certiconform(){
       }else{
         $.each(r, function(i,e){
           let tmp_dataformatinput = "";
-          tmp_dataformatinput = "withcomedecimal";
-          tmp += `
-            <tr id="item-${e.id}">
-              <td>
-                <div class="cont-dashCamel__cControlsList--cC--cTable--cControl">
-                  <label for="" class="cont-dashCamel__cControlsList--cC--cTable--cControl--label">
-                    <a class="btn-update-certconform" href="javascript:void(0);" data-toggle="modal" data-target="#updateModalAduanas" 
-                       data-id="${e.id}"
-                       data-dataname="${e.data_name}"
-                       data-datavalue="${e.data_value}"
-                       data-dataformat="${tmp_dataformatinput}">
-                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" style="enable-background:new 0 0 100 100;" xml:space="preserve"><path d="M90.8,99H9.2C4.7,99,1,95.3,1,90.8V9.2c0-4.5,3.7-8.2,8.2-8.2h52.3l-8.2,8.2H9.2v81.6h81.6V46.7l8.2-8.2v52.3  C99,95.3,95.3,99,90.8,99z M41.8,78.6l-20.4,0.1l0-20.5L76.7,3.3c3.1-3.1,8.2-3.1,11.3,0c0,0,0,0,0,0l8.5,8.5  c3.1,3.1,3.1,8.2,0,11.4c0,0,0,0,0,0L41.8,78.6z M29.6,70.4h12.2L29.6,58.2V70.4z M82.4,9L37.8,54.1l8.2,8.2l45-44.7L82.4,9z"/></svg>
-                    </a>
-                    <span>${e.data_name}</span>
-                    <span>*</span>
-                  </label>
-                  <div class="cont-dashCamel__cControlsList--cC--cTable--cControl--cInputs mx-longlabel">
-                    <input type="text" data-valformat='withcomedecimal' id="" name="" placeholder="0.00" class="cont-dashCamel__cControlsList--cC--cTable--cControl--cInputs--input" value="${e.data_value}" step="0.01">
+          if(e.id == 1){
+            tmp_dataformatinput = "withcomedecimal";
+            tmp += `
+              <tr id="item-${e.id}">
+                <td>
+                  <div class="cont-dashCamel__cControlsList--cC--cTable--cControl">
+                    <label for="" class="cont-dashCamel__cControlsList--cC--cTable--cControl--label">
+                      <a class="btn-update-certconform" href="javascript:void(0);" data-toggle="modal" data-target="#updateModalAduanas" 
+                         data-id="${e.id}"
+                         data-dataname="${e.data_name}"
+                         data-datavalue="${e.data_value}"
+                         data-dataformat="${tmp_dataformatinput}">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" style="enable-background:new 0 0 100 100;" xml:space="preserve"><path d="M90.8,99H9.2C4.7,99,1,95.3,1,90.8V9.2c0-4.5,3.7-8.2,8.2-8.2h52.3l-8.2,8.2H9.2v81.6h81.6V46.7l8.2-8.2v52.3  C99,95.3,95.3,99,90.8,99z M41.8,78.6l-20.4,0.1l0-20.5L76.7,3.3c3.1-3.1,8.2-3.1,11.3,0c0,0,0,0,0,0l8.5,8.5  c3.1,3.1,3.1,8.2,0,11.4c0,0,0,0,0,0L41.8,78.6z M29.6,70.4h12.2L29.6,58.2V70.4z M82.4,9L37.8,54.1l8.2,8.2l45-44.7L82.4,9z"/></svg>
+                      </a>
+                      <span>${e.data_name}</span>
+                      <span>*</span>
+                    </label>
+                    <div class="cont-dashCamel__cControlsList--cC--cTable--cControl--cInputs mx-longlabel">
+                      <input type="text" data-valformat='withcomedecimal' id="" name="" placeholder="0.00" class="cont-dashCamel__cControlsList--cC--cTable--cControl--cInputs--input" value="${e.data_value}" step="0.01">
+                    </div>
                   </div>
-                </div>
-              </td>
-            </tr>`;
+                </td>
+              </tr>`;
+          }else{
+            tmp_dataformatinput = "onlyintegers";
+            tmp += `
+              <tr id="item-${e.id}">
+                <td>
+                  <div class="cont-dashCamel__cControlsList--cC--cTable--cControl">
+                    <label for="" class="cont-dashCamel__cControlsList--cC--cTable--cControl--label">
+                      <a class="btn-update-certconform" href="javascript:void(0);" data-toggle="modal" data-target="#updateModalAduanas" 
+                         data-id="${e.id}"
+                         data-dataname="${e.data_name}"
+                         data-datavalue="${e.data_value}"
+                         data-dataformat="${tmp_dataformatinput}">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" style="enable-background:new 0 0 100 100;" xml:space="preserve"><path d="M90.8,99H9.2C4.7,99,1,95.3,1,90.8V9.2c0-4.5,3.7-8.2,8.2-8.2h52.3l-8.2,8.2H9.2v81.6h81.6V46.7l8.2-8.2v52.3  C99,95.3,95.3,99,90.8,99z M41.8,78.6l-20.4,0.1l0-20.5L76.7,3.3c3.1-3.1,8.2-3.1,11.3,0c0,0,0,0,0,0l8.5,8.5  c3.1,3.1,3.1,8.2,0,11.4c0,0,0,0,0,0L41.8,78.6z M29.6,70.4h12.2L29.6,58.2V70.4z M82.4,9L37.8,54.1l8.2,8.2l45-44.7L82.4,9z"/></svg>
+                      </a>
+                      <span>${e.data_name}</span>
+                      <span>*</span>
+                    </label>
+                    <div class="cont-dashCamel__cControlsList--cC--cTable--cControl--cInputs mx-longlabel">
+                      <input type="text" data-valformat='onlyintegers' id="" name="" placeholder="0.00" class="cont-dashCamel__cControlsList--cC--cTable--cControl--cInputs--input" value="${e.data_value}">
+                    </div>
+                  </div>
+                </td>
+              </tr>`;
+          }
         });      
         $("#tbl_othervalues_cert_conform").html(tmp);
       }
